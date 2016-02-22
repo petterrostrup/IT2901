26e486fe495dff31281e798418ac0ec85a332800
mrwe <erikwik@stud.ntnu.no>
2016-02-22 11:16:59 +0100

    Start support for multiple languages

---
+++ client/index.js
@@ -0,0 +1,38 @@
+getUserLanguage = function () {
+    // Put logic for determining the user language
+    return 'en'
+};
+
+if (Meteor.isClient) {
+    Meteor.startup(function () {
+        Session.set("showLoadingIndicator", true);
+
+        TAPi18n.setLanguage(getUserLanguage())
+            .done(function () {
+                Session.set("showLoadingIndicator", false);
+            })
+            .fail(function (error_message) {
+                // Handle the situation
+                console.log(error_message);
+            });
+    });
+}
+
+Template.navigation.events = {
+    'click #language': function(){
+        if(TAPi18n.getLanguage() == 'en'){
+            var lang = 'no';
+        }
+        else{
+            var lang = 'en';
+        }
+        TAPi18n.setLanguage(lang)
+            .done(function () {
+                Session.set("showLoadingIndicator", false);
+            })
+            .fail(function (error_message) {
+                // Handle the situation
+                console.log(error_message);
+            });
+    }
+}
