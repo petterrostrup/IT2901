0f8eaa9c470919f31cbf62a951fd6ff7376c0f53
petterrostrup <petterrostrup@gmail.com>
2016-04-06 11:36:07 +0200

    Fixed homepage. Now serious and content is linked. Forgot to branch, sorry

--- client/pages/home/home.js
+++ client/pages/home/home.js
@@ -1,5 +1,11 @@
 Template.home.helpers({
 	getContent: function() {
 		return Content.find({});
+	},
+
+	getCategory: function(catId) {
+
+		var cat = Category.findOne({_id: catId});
+		return cat.name;
 	}
 });
