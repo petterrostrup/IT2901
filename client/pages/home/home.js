fcc443b979d2fa62776fc8781a69b1a8e84436aa
Jørgen Halvorsen <jorgen-halvorsen@hotmail.com>
2016-04-25 18:30:34 +0200

    Fixed error with news feed

--- client/pages/home/home.js
+++ client/pages/home/home.js
@@ -1,17 +1,57 @@
 Template.home.helpers({
 	getContent: function() {
-		return Content.find({});
+		var default_language = Session.get("current_language");
+		default_language = LanguageTags.findOne({
+			short_form: default_language
+		});
+
+		var content = ContentText.find({
+			language: default_language.name
+		},{
+			limit: 10,
+			sort: {createdAt: -1}
+		});
+		return content;
+
+		// if (!Meteor.userId()) {
+		// 	var content = Content.find({
+		// 		limit: 10
+		// 	});
+		// 	content.reverse();
+		// 	return content;
+		// }
+
+		// var user_langs = Meteor.user().profile.languages;
+		// var default_language = Session.get("current_language");
+		// default_language = LanguageTags.findOne({
+		// 	short_form: default_language
+		// });
+		// var content = [];
+		// var found = false;
+		// for (var a in user_langs) {
+		// 	if (user_langs[a] === default_language._id){
+		// 		user_langs[a] = default_language;
+		// 		found = true;
+		// 	}else {
+		// 		user_langs[a] = LanguageTags.findOne({
+		// 			_id: user_langs[a]
+		// 		});
+		// 	}
+		// }
+		// if (!found) 
+		// 	user_langs.push(default_language);
+		// var content = [];
 	},
 
 	getCategory: function(catId) {
-
-		var cat = Category.findOne({_id: catId});
+		var content = Content.findOne({_id: catId});
+		var cat = Category.findOne({_id: content.category_id});
 		return cat.name;
 	},
 
 	getIcon: function(catId) {
-
-		var cat = Category.findOne({_id: catId});
+		var content = Content.findOne({_id: catId});
+		var cat = Category.findOne({_id: content.category_id});
 		return cat.icon;
 	},
 
