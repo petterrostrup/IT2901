b0239e1ffabda27898224ce7f03932edd8144292
halv00rsen <jorgen-halvorsen@hotmail.com>
2016-05-22 15:59:26 +0200

    Updated README and commented the server side code

--- server/security.js
+++ server/security.js
@@ -1,6 +1,8 @@
 
+// Not in use right now
+
 // Only creators can insert and update the Content database.
-Content.permit(["insert", "update"]).ifHasRole("creator").apply();
+// Content.permit(["insert", "update"]).ifHasRole("creator").apply();
 
 // Only current user can update his own information in the database
 // Meteor.users.permit(['update']).ifIsCurrentUser().onlyProps(['services.password', 'profile']);
